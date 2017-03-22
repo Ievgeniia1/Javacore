05a11e9588b8d63ff873cb67cfb77bc21d5a1bbd
Ievgeniia Ganzina <evgeniya.ganzina@gmail.com>
2017-03-23 00:37:44 +0200

    Unit tests - Classwork edited

--- src/test/java/com/ganzina/app/classwork/lesson3/MathFuncTest.java
+++ src/test/java/com/ganzina/app/classwork/lesson3/MathFuncTest.java
@@ -8,24 +8,22 @@ import org.junit.Test;
  * Created by Ievgeniia on 3/15/2017.
  */
 public class MathFuncTest {
+
     @Test
     public void multiplyTest(){
     int a = 1;
     int b = 2;
-    MathFunc mathFunc = new MathFunc();
-    int actualResult = mathFunc.multiply(a,b);
     int expectedResult = 2;
-    Assert.assertEquals(expectedResult,actualResult);
+    Assert.assertEquals(expectedResult,MathFunc.multiply(a,b));
 
     }
     @Test
     public void sumTest(){
-        int c=5;
-        int d=3;
-        MathFunc mathFunc = new MathFunc();
-        int actualResult = mathFunc.sum(c,d);
-        int expectedResult = 8;
-        Assert.assertEquals(expectedResult,actualResult);
+        int a = 1;
+        int b = 2;
+        int expectedResult = 3;
+        Assert.assertEquals(expectedResult,MathFunc.sum(a,b));
+
     }
 
 
