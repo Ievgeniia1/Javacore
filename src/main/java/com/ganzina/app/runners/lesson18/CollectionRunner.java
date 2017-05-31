0dce24e4201f9114a8e8c1d5636201794cac9914
Ievgeniia Ganzina <evgeniya.ganzina@gmail.com>
2017-06-01 00:58:23 +0300

    removed files

--- src/main/java/com/ganzina/app/runners/lesson18/CollectionRunner.java
+++ src/main/java/com/ganzina/app/runners/lesson18/CollectionRunner.java
@@ -1,6 +1,6 @@
 package com.ganzina.app.runners.lesson18;
 
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by Ievgeniia on 5/23/2017.
@@ -13,6 +13,17 @@ public class CollectionRunner {
         arrayList.add(bmw);
         arrayList.add(audi);
         System.out.println(arrayList);
+
+        Map map = new HashMap<>();
+        map.put("KIT",13);
+        map.put("Ciklum",15);
+        Set entries=map.entrySet();
+        System.out.println(entries);
+        Set keys = map.keySet();
+        System.out.println(keys);
+        System.out.println(map.containsKey("Ciklum"));
+        System.out.println(map.containsValue(13));
+
 
     }
 }
